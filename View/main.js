ae60b3a0a88eb08d31c71f452a27a88fdb564cb0
Gabriel M <gabrieletudes@gmail.com>
2017-12-24 14:15:34 +0100

    Added Login and Jury routes

--- View/main.js
+++ View/main.js
@@ -1,7 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter);
+
+//import the different views
+import Jury from './components/Jury.vue'
+import Login from './components/Login.vue'
+//Define the routes that can be viewed
+const router = new VueRouter({
+  mode: 'history',
+  routes:[
+    {path:'/', component: Login},
+    {path:'/jury',component: Jury}
+    ]
+})
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
