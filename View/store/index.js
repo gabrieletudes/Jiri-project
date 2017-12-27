6caacdde696bd17a6c6784f6b833d4b2b9364c09
Gabriel M <gabrieletudes@gmail.com>
2017-12-27 23:41:45 +0100

    store index for vuex

---
+++ View/store/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import { getters } from './getters.js'
+import { mutations } from './mutations.js'
+import { actions } from './actions.js'
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+    state: {
+        allStudents: [],
+        student:[]
+    },
+    getters,
+    mutations,
+    actions,
+})
