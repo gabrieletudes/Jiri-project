9996932520d6bf0c8741446c41548a973c1fec1f
Gabriel M <gabrieletudes@gmail.com>
2018-01-07 22:34:25 +0100

    Added login page and authentification

--- View/components/Login.vue
+++ View/components/Login.vue
@@ -1,10 +1,40 @@
 <template>
   <div>
-      Ceci est la page de login
+      <b-field label="Email">
+      <b-input type="email"
+              v-model="email"
+              placeholder="johndoe@email.com"
+              >
+          </b-input>
+      </b-field>
+      <b-field label="Password">
+            <b-input
+            v-model="password"
+            placeholder="">
+            </b-input>
+        </b-field>
+        <button type="button" @click="authenticateUser" class="button is-primary">Se connecter</button>
   </div>
 </template>
 <script>
+import EventBus from '../event-bus'
 export default {
-  
+  name: 'AppLogin',
+  data () {
+    return {
+      email: null,
+      password: null,
+      login: true,
+    }
+  },
+  methods: {
+    authenticateUser(){
+      let {email, password} = this;
+      console.log(this.email + ' ' + this.password)
+      EventBus.$emit('authenticateUser',{email, password});
+      this.email = '';
+      this.password = '';
+    },
+  }
 }
 </script>
