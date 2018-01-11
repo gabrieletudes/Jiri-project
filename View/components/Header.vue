c84177926e54bacaa93ccc08e6c9969d18e1eb42
Gabriel M <gabrieletudes@gmail.com>
2018-01-11 10:09:45 +0100

    Added header component

---
+++ View/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+  <nav class="navbar is-link">
+  <div class="navbar-brand">
+    <a class="navbar-item" :href="'/'">
+      Jiri
+    </a>
+    <div class="navbar-burger burger" data-target="navMenuColorlink-example">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </div>
+  <div id="navMenuColorlink-example" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item" :href="'/jury'">
+        Home
+      </a>
+    </div>
+    <div class="navbar-end">
+      <div class="navbar-item">
+          <div class="button is-primary" @click="logout()">logout</div>
+      </div>
+    </div>
+  </div>
+</nav>
+</div>
+</template>
+<script>
+export default {
+  name: 'AppHeader',
+  computed: {
+    userId () {
+      return this.$root.$data.userId
+    }
+  },
+  methods: {
+    logout () {
+      localStorage.removeItem('userToken')
+      this.$router.push({name: 'login'})
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
