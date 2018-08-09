726738f67a7a3eb2f61e10dcdb96d4a477f27113
Gabriel M <gabrieletudes@gmail.com>
2018-08-09 19:54:11 +0200

    Removed semicolon after import

--- View/main.js
+++ View/main.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import {apolloProvider} from './apollo'
 import {store} from './store'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
 
 Vue.use(ElementUI);
