6a3c9d4e0ec3090a5fae4328fe1cfac71d954e97
Gabriel M <gabrieletudes@gmail.com>
2018-08-09 22:58:00 +0200

    Removed some styling from App component

--- View/App.vue
+++ View/App.vue
@@ -19,28 +19,10 @@ export default {
 }
 </script>
 
-<style lang="scss" src="bulma">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-}
-
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
 }
 </style>
