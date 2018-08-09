4cbf0cb655f257a71c3cc45b993012e3091d9d71
Gabriel M <gabrieletudes@gmail.com>
2018-08-09 19:59:22 +0200

    Added side menu

---
+++ View/components/SideMenu.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-aside class="aside">
+    <el-menu class="aside__menu">
+      <el-menu-item index="1">Jurys</el-menu-item>
+      <el-menu-item index="2">Etudiants</el-menu-item>
+      <el-menu-item index="3">Membres du jury</el-menu-item>
+      <el-menu-item index="4">Projets</el-menu-item>
+    </el-menu>
+  </el-aside>
+</template>
+<script>
+export default {
+  name: "SideMenu",
+}
+</script>
+<style lang="scss" scoped>
+.aside {
+  max-width: 200px;
+  background-color: white;
+  &__menu {
+    border-right: none;
+  }
+}
+</style>
