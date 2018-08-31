f35d324ceb3bee33593a0032215ae94e65eb2aaf
Gabriel M <gabrieletudes@gmail.com>
2018-08-31 04:36:37 +0200

    Added general classes

--- View/App.vue
+++ View/App.vue
@@ -20,8 +20,10 @@ export default {
 </script>
 
 <style lang="scss">
-body {
+body, html{
   height: 100%;
+}
+body {
   margin: 0;
   background-color: #F4F6F9;
 }
@@ -30,5 +32,34 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  height: 100%;
+}
+.el-container{
+  &--wrap {
+    height: 100%;
+  }
+}
+.el-main {
+  padding-bottom: 100px;
+  padding-left: 32px;
+  padding-right: 32px;
+}
+.hide-visually {
+    border: 0;
+    clip: rect(0 0 0 0);
+    clip-path: inset(50%);
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+    white-space: nowrap;
+    width: 1px;
+}
+h3 {
+  margin-bottom: 8px;
+}
+h3 + p {
+  margin-top: 8px;
 }
 </style>
