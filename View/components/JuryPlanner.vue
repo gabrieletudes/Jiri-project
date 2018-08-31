4c1d91dbc43214e97a0f3e2521bbcba1a06c7d02
Gabriel M <gabrieletudes@gmail.com>
2018-08-31 05:56:01 +0200

    Added JuryPlanner component containing the steps for the event

---
+++ View/components/JuryPlanner.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-main>
+    <el-steps :active="2" align-center>
+      <el-step title="Step 1" description="Some description"></el-step>
+      <el-step title="Step 2" description="Some description"></el-step>
+      <el-step title="Step 3" description="Some description"></el-step>
+      <el-step title="Step 4" description="Some description"></el-step>
+    </el-steps>
+  </el-main>
+</template>
+<script>
+export default {
+  name: "JuryPlanner"
+}
+</script>
+<style lang="scss" scoped>
+</style>
