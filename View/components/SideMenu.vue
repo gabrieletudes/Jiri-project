9b96d17d733a17f9e6f140cb07f51a70904ffe11
Gabriel M <gabrieletudes@gmail.com>
2018-08-31 06:16:03 +0200

    Added router link to SideMenu

--- View/components/SideMenu.vue
+++ View/components/SideMenu.vue
@@ -1,10 +1,21 @@
 <template>
   <el-aside class="aside">
     <el-menu class="aside__menu">
-      <el-menu-item index="1">Jurys</el-menu-item>
-      <el-menu-item index="2">Etudiants</el-menu-item>
-      <el-menu-item index="3">Membres du jury</el-menu-item>
-      <el-menu-item index="4">Projets</el-menu-item>
+      <el-menu-item index="1">
+        <router-link :to="{ name: 'jury' }" class="el-menu-item__link" exact>Jiri</router-link>
+      </el-menu-item>
+      <el-menu-item index="2">
+        <router-link :to="{ name: 'jurys' }" class="el-menu-item__link">Jurys</router-link>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <router-link :to="{ name: 'students' }" class="el-menu-item__link">Étudiants</router-link>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <router-link :to="{ name: 'members' }" class="el-menu-item__link">Membres</router-link>
+      </el-menu-item>
+      <el-menu-item index="5">
+        <router-link :to="{ name: 'projects' }" class="el-menu-item__link">Projets</router-link>
+      </el-menu-item>
     </el-menu>
   </el-aside>
 </template>
